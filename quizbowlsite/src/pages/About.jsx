44fb06191b70736c80c49ac063df708f4610cdad
jay2002shah <shahj@oregonstate.edu>
2024-03-13 09:39:53 -0700

    #added some content

--- quizbowlsite/src/pages/About.jsx
+++ quizbowlsite/src/pages/About.jsx
@@ -1,3 +1,24 @@
 export function About() {
-  return <h1>About 4-H Extension</h1>;
+  return (
+    <div>
+  <h1>About 4-H Extension</h1>
+  <h2>Websites Purpose:</h2>
+  <ul>
+    <li>
+      <h3>This website allows user of the quizbowl to access quiz questions through basic filtering</h3>
+    </li>
+  </ul>
+  <h2>4-H Program Information:</h2>
+  <ul>
+    <li>
+      <h3>Youth Program that is an extension provided by Oregon State University</h3>
+    </li>
+  </ul>
+
+  </div>
+
+  );
+
+  
+
 }
