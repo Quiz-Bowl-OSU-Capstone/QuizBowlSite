705539ef40a01ae7be828789c203535f151d09fa
jay2002shah <shahj@oregonstate.edu>
2024-03-14 21:55:37 -0700

    #added more in the about

--- quizbowlsite/src/pages/About.jsx
+++ quizbowlsite/src/pages/About.jsx
@@ -6,6 +6,8 @@ export function About() {
   <ul>
     <li>
       <h3>This website allows user of the quizbowl to access quiz questions through basic filtering</h3>
+      <h3>Users are required to login before accessing the filtering can happen on this site.</h3>
+      <h3>Based of off what the user filters, the backend will run to the Azure databse which will fetch questions based on the filters.</h3>
     </li>
   </ul>
   <h2>4-H Program Information:</h2>
@@ -14,6 +16,37 @@ export function About() {
       <h3>Youth Program that is an extension provided by Oregon State University</h3>
     </li>
   </ul>
+
+  <h2>Website Technical Information:</h2>
+  <ul>
+    <li>
+      <h3>This is the front-end web interface for the Quiz Bowl database.</h3>
+      <li>
+      <h3>Azure Static Web App</h3>
+      </li>
+     
+    </li>
+  </ul>
+
+
+  <h2>Languages Used: </h2>
+  <ul>
+      <li>
+        <h3>
+          Next.js(framework for React.js)
+        </h3>
+      </li>
+ 
+  </ul>
+
+  <h2>Internal File Structure</h2>
+  <ul>
+    <li>
+      <h3>github to quizbowlsite</h3>
+      <h3>The src folder contains the code for this static webapp</h3>      
+    </li>
+  </ul>
+  
 
   </div>
 
