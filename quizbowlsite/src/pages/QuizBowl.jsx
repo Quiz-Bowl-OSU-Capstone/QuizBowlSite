b954b61ed2e8bcd56640d874d8360f62e96a531b
Aura <33647317+ActuallyCloud@users.noreply.github.com>
2024-04-15 19:02:20 -0700

    Minor CSS changes and styling fixes

--- quizbowlsite/src/pages/QuizBowl.jsx
+++ quizbowlsite/src/pages/QuizBowl.jsx
@@ -49,6 +49,9 @@ export function QuizBowl() {
     // Edit
   }
 
+  function handleRemoveClick() {
+  }
+
   // Function to handle delete button click
   function handleDeleteClick() {
     // Add
@@ -126,112 +129,112 @@ export function QuizBowl() {
   }, []);
 
   return (
-    <div className="content-holder">
-      <aside className="sidebar">
-        <div className="filter-box">
-          <h3 style={{ textAlign: "center" }}>Important Information</h3>
-          <p>
-            If this is the first time in a while that you are using this
-            website, it may take a long time to load initially. This is normal
-            and it should be faster afterwards
-          </p>
-          <p>
-            You need to log into the website in order to load questions on this
-            page. If you are not logged in, please do so now.
-          </p>
-          <h3 style={{ textAlign: "center" }}>Filters</h3>
-          <p>
-            Select checkboxes to enable/disable filters. Use drop down menus to
-            adjust filter settings.
-          </p>
-          <form>
-            <ul>
-              <li>
-                <label htmlFor="level">
-                  Level
-                  <br />
-                </label>
-                <input
-                  type="checkbox"
-                  className="inputbox"
-                  id="level-enabled"
-                  name="Level"
-                />
-                <select id="level" className="select-box">
-                  {filters.level.map((level, index) => (
-                    <option key={index} value={level}>
-                      {level}
-                    </option>
-                  ))}
-                </select>
-              </li>
-              <li>
-                <label htmlFor="species">
-                  Species
-                  <br />
-                </label>
-                <input
-                  type="checkbox"
-                  className="inputbox"
-                  id="species-enabled"
-                  name="Species"
-                />
-                <select id="species" className="select-box">
-                  {filters.species.map((species, index) => (
-                    <option key={index} value={species}>
-                      {species}
-                    </option>
-                  ))}
-                </select>
-              </li>
-              <li>
-                <label htmlFor="resource">
-                  Resource
-                  <br />
-                </label>
-                <input
-                  type="checkbox"
-                  className="inputbox"
-                  id="resource-enabled"
-                  name="Resource"
-                />
-                <select id="resource" className="select-box">
-                  {filters.resource.map((resource, index) => (
-                    <option key={index} value={resource}>
-                      {resource}
-                    </option>
-                  ))}
-                </select>
-              </li>
-              <li>
-                <label htmlFor="topic">
-                  Topic
-                  <br />
-                </label>
-                <input
-                  type="checkbox"
-                  className="inputbox"
-                  id="topic-enabled"
-                  name="Topic"
-                />
-                <select id="topic" className="select-box">
-                  {filters.topic.map((topic, index) => (
-                    <option key={index} value={topic}>
-                      {topic}
-                    </option>
-                  ))}
-                </select>
-              </li>
-            </ul>
-          </form>
-          <button id="gen-questions" onClick={handleClick}>
-            Generate Questions
-          </button>
-        </div>
-      </aside>
+    <div className="main-holder">
+      <div className="content-holder">
+        <aside className="sidebar">
+          <div className="filter-box">
+            <h3 style={{ textAlign: "center" }}>Important Information</h3>
+            <p>
+              If this is the first time in a while that you are using this
+              website, it may take a long time to load initially. This is normal
+              and it should be faster afterwards
+            </p>
+            <p>
+              You need to log into the website in order to load questions on this
+              page. If you are not logged in, please do so now.
+            </p>
+            <h3 style={{ textAlign: "center" }}>Filters</h3>
+            <p>
+              Select checkboxes to enable/disable filters. Use drop down menus to
+              adjust filter settings.
+            </p>
+            <form>
+              <ul>
+                <li>
+                  <label htmlFor="level">
+                    Level
+                    <br />
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="inputbox"
+                    id="level-enabled"
+                    name="Level"
+                  />
+                  <select id="level" className="select-box">
+                    {filters.level.map((level, index) => (
+                      <option key={index} value={level}>
+                        {level}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+                <li>
+                  <label htmlFor="species">
+                    Species
+                    <br />
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="inputbox"
+                    id="species-enabled"
+                    name="Species"
+                  />
+                  <select id="species" className="select-box">
+                    {filters.species.map((species, index) => (
+                      <option key={index} value={species}>
+                        {species}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+                <li>
+                  <label htmlFor="resource">
+                    Resource
+                    <br />
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="inputbox"
+                    id="resource-enabled"
+                    name="Resource"
+                  />
+                  <select id="resource" className="select-box">
+                    {filters.resource.map((resource, index) => (
+                      <option key={index} value={resource}>
+                        {resource}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+                <li>
+                  <label htmlFor="topic">
+                    Topic
+                    <br />
+                  </label>
+                  <input
+                    type="checkbox"
+                    className="inputbox"
+                    id="topic-enabled"
+                    name="Topic"
+                  />
+                  <select id="topic" className="select-box">
+                    {filters.topic.map((topic, index) => (
+                      <option key={index} value={topic}>
+                        {topic}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+              </ul>
+            </form>
+            <button id="gen-questions" onClick={handleClick}>
+              Generate Questions
+            </button>
+          </div>
+        </aside>
+      </div>
       <div className="question-holder">
-        <h2 className="content-container">Questions</h2>
-
         {randomQuestions.map((question, index) => (
           <div
             key={index}
@@ -242,18 +245,22 @@ export function QuizBowl() {
               <strong>{question.Question}</strong>
               <br />
               Answer: {question.Answer}
+              <br />
+              <i>Level: {question.Level} | Species: {question.Species} | Topic:{" "}
+                  {question.Topic}</i>
             </p>
             {/* Additional information that is shown when a card is clicked */}
             {selectedQuestion && selectedQuestion.id === question.id && (
-              <div>
+              <div className="question-info-holder">
                 <p>
-                  Level: {question.Level} | Species: {question.Species} | Topic:{" "}
-                  {question.Topic} | Resource: {question.Resource} | ID:{" "}
+                  Resource: {question.Resource} | ID:{" "}
                   {question.id}
                 </p>
                 <div className="action-buttons">
                   {/* Edit button */}
                   <button onClick={handleEditClick}>Edit</button>
+                  {/* Remove button */}
+                  <button onClick={handleRemoveClick}>Remove</button>
                   {/* Delete button */}
                   <button onClick={handleDeleteClick}>Delete</button>
                 </div>
