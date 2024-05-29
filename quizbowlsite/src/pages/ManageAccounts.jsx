766d1b1bf0a7f29d474edae8905847f8368a2878
Aura <33647317+ActuallyCloud@users.noreply.github.com>
2024-05-28 17:56:02 -0700

    Finished implementing account management

--- quizbowlsite/src/pages/ManageAccounts.jsx
+++ quizbowlsite/src/pages/ManageAccounts.jsx
@@ -5,6 +5,8 @@ export function ManageAccounts() {
     const [cookies, setCookie, removeCookie] = useCookies(["user"]);
     const [accounts, setAccounts] = React.useState([]);
     const [selectedAccount, setSelectedAccount] = useState(null);
+    const [actionableAccount, setActionableAccount] = useState(null);
+    const [action, setAction] = useState(null);
 
     var handleAccountClick = (username) => {
         if (selectedAccount == username) {
@@ -20,6 +22,12 @@ export function ManageAccounts() {
         }
     }
 
+    var handleEnterKeyPopup = (event) => {
+        if (event.key === "Enter") {
+            handlePopupClose();
+        }
+    }
+
     var handleEnterKeyCreate = (event) => {
         if (event.key === "Enter") {
             handleCreateAccount(document.getElementById("password").value);
@@ -27,13 +35,35 @@ export function ManageAccounts() {
     }
 
     async function getAccounts(pass) {
+        document.getElementById("login-loading").style.display = "flex";
+        document.getElementById("password").setAttribute("disabled", "true");
+        document.getElementById("relogin").setAttribute("disabled", "true");
+
         const response = await fetch("https://qzblapi.azurewebsites.net/api/ListAccounts?uid=" + cookies.auth.uid + "&currentpass=" + encodeURIComponent(pass));
-        const data = await response.json();
-        console.log(data)
-        setAccounts(data.accounts);
+        if (!response.ok) {
+            alert("Failed to authenticate. Please try again.");
+            throw new Error("Failed to authenticate");
+        }
+        var data = await response.json();
+        if (data.Error) {
+            alert("Failed to authenticate. Please try again.");
+            
+            document.getElementById("login-loading").style.display = "none";
+            document.getElementById("password").removeAttribute("disabled");
+            document.getElementById("relogin").removeAttribute("disabled");
+            return;
+        }
+        var accounts = data.accounts.sort();
+        setAccounts(accounts);
     }
 
     async function handleCreateAccount(currentpass) {
+        document.getElementById("newusername").setAttribute("disabled", "true");
+        document.getElementById("newpassword").setAttribute("disabled", "true");
+        document.getElementById("newadmin").setAttribute("disabled", "true");
+        document.getElementById("password").setAttribute("disabled", "true");
+        document.getElementById("button-create-account").setAttribute("disabled", "true");
+
         var newusername = document.getElementById("newusername").value;
 
         if (newusername == "") {
@@ -56,15 +86,87 @@ export function ManageAccounts() {
             if (data.Error) {
                 alert(data.Error);
             } else {
-                alert("Account created successfully!");
+                alert("Created the new account successfully! The password to log into the new account is '" + data.password + "'. Please inform the user of this new password, and make sure to write this down or save it somewhere, as this information won't be shown again.");
                 getAccounts(currentpass);
+            }
+        }
+
+        document.getElementById("newusername").removeAttribute("disabled");
+        document.getElementById("newpassword").removeAttribute("disabled");
+        document.getElementById("newadmin").removeAttribute("disabled");
+        document.getElementById("password").removeAttribute("disabled");
+        document.getElementById("button-create-account").removeAttribute("disabled");
+    }
+
+    async function handlePasswordConfirm(username, action) {
+        setAction(action);
+        setActionableAccount(username);
+    }
+
+    function handlePopupClose() {
+        document.getElementById("popup-password").setAttribute("disabled", "true");
+        document.getElementById("button-reset-delete").setAttribute("disabled", "true");
+
+        if (document.getElementById("popup-password").value == "") {
+            alert("You need to enter your password to confirm this action.");
+            document.getElementById("popup-password").removeAttribute("disabled");
+            document.getElementById("button-reset-delete").removeAttribute("disabled");
+        } else {
+            if (action == "reset") {
+                handleResetPassword(actionableAccount, document.getElementById("popup-password").value);
+            } else {
+                handleAccountDelete(actionableAccount, document.getElementById("popup-password").value);
             }
         }
     }
 
-    async function handleResetPassword(username) {}
+    async function handleResetPassword(username, pass) {
+        var params = "?uid=" + cookies.auth.uid + "&currentpass=" + encodeURIComponent(pass) + "&acctusername=" + username;
+        var newpass = prompt("Did you want to set a specific password for this account? If so, you can enter that here. Otherwise, leave this blank to generate a random password. Clicking 'Cancel' will cancel the password reset.")
+        
+        if (newpass != null) {
+            if (newpass.length >= 8) {
+                params += "&newpassword=" + newpass;
+            } else if (newpass.length < 8 && newpass.length > 0) {
+                alert("Password must be at least 8 characters long.");
+                return;
+            }
 
-    async function handleAccountDelete(username) {}
+            const response = await fetch("https://qzblapi.azurewebsites.net/api/ChangePassword" + params);
+            if (!response.ok) {
+                alert("Failed to reset password for account.");
+                throw new Error("Failed to fetch user details");
+            }
+            var data = await response.json();
+
+            alert("Password reset successfully! The new password is '" + data.pass + "'. Please inform the user of this new password, and make sure to write this down or save it somewhere, as this information won't be shown again.");
+            setActionableAccount(null);
+            document.getElementById("popup-password").removeAttribute("disabled");
+            document.getElementById("button-reset-delete").removeAttribute("disabled");
+        } else {
+            setActionableAccount(null);
+            setAction(null);
+            document.getElementById("popup-password").removeAttribute("disabled");
+            document.getElementById("button-reset-delete").removeAttribute("disabled");
+        }
+    }
+
+    async function handleAccountDelete(username, pass) {
+        var params = "?uid=" + cookies.auth.uid + "&currentpass=" + encodeURIComponent(pass) + "&username=" + username;
+
+        const response = await fetch("https://qzblapi.azurewebsites.net/api/RemoveAccount" + params);
+        if (!response.ok) {
+            throw new Error("Failed to fetch user details");
+            alert("Failed to delete account.");
+        }
+
+        var data = await response.json();
+        alert("Account deleted successfully!");
+
+        setActionableAccount(null);
+        setAction(null);
+        getAccounts(pass);
+    }
 
     function AccountDisplay({}) {
         if (accounts.length == 0) {
@@ -74,11 +176,52 @@ export function ManageAccounts() {
                     <label>
                         <input className="loginbox" type="password" id="password" placeholder="Password" onKeyDown={handleEnterKey}/>
                     </label>
+                    <button id="relogin" className="mainbutton" onClick={() => getAccounts(document.getElementById("password").value)}>OK</button>    
                 </div>
             );
         } else {
             return (
                 <div>
+                    <hr />
+                    {actionableAccount != null? (
+                        <div id="popup-pass" className="dialog-short">
+                            {action == "reset" ? (
+                                <h3>Do you want to reset the password for '{actionableAccount}'?</h3>
+                            ) : (
+                                <h3>Do you want to delete the account '{actionableAccount}'?</h3>
+                            )}
+                            <input id="popup-password" className="loginbox" type="password" onKeyDown={handleEnterKeyPopup} placeholder="Enter your password to confirm"/>
+                            <button id="button-reset-delete" className="mainbutton" onClick={() => handlePopupClose()}>OK</button>
+                        </div>
+                    ) : (
+                        <div className="dialog-short">
+                            <div>
+                                <h3>Want to create a new account?</h3>
+                                <p>First, fill out the fields below. You need to provide a unique username to identify the account. Providing a password is optional (if none is provided, a random one will be generated).</p>
+                                <label>
+                                    <input type="text" id="newusername" placeholder="New Username"/>
+                                </label>
+                                <label>
+                                    <input type="password" id="newpassword" placeholder="New Password"/>
+                                </label>
+                            </div>
+                            <div>
+                                <h4>Do you want this account to be an administrator?</h4>
+                                <p>Administrators can edit, create, and delete questions.
+                                Administrators can also manage other accounts using this page.
+                                Grant this permission carefully!</p>
+                                <label>
+                                    <input type="checkbox" id="newadmin" className="select-box"/>
+                                </label>
+                            </div>
+                            <div>
+                                <p>In order to create a new account, please re-enter your password and click "Create Account".</p>
+                                <input className="loginbox" type="password" id="password" placeholder="Password" onKeyDown={handleEnterKeyCreate}/>
+                                <button id="button-create-account" className="mainbutton" onClick={() => handleCreateAccount(document.getElementById("password").value)}>Create Account</button>
+                            </div>
+                        </div>
+                    )}
+                    <hr />
                     {accounts.map((account, index) => (
                     <div
                         key={index}
@@ -89,36 +232,17 @@ export function ManageAccounts() {
 
                         {selectedAccount == account.username ? (
                         <div className="question-info-holder">
-                            <button className="mainbutton" onClick={() => handleResetPassword(account.username)}>Reset Password</button>
-                            <button className="mainbutton" onClick={() => handleAccountDelete(account.username)}>Delete Account</button>
+                            <button className="mainbutton" onClick={() => handlePasswordConfirm(account.username, "reset")}>Reset Password</button>
+                            <button className="mainbutton" onClick={() => handlePasswordConfirm(account.username, "delete")}>Delete Account</button>
                         </div>
-                        ) : "Administrator: " + account.admin.toString().toUpperCase()}
+                        ) : (
+                            <p>
+                                {account.admin ? <strong>Administrator</strong> : "User"}
+                            </p>
+                        )}
                     </div>
                     ))}
                     <hr />
-                    <h3>Want to create a new account?</h3>
-                    <p>Fill out the fields below, then click Create Account.</p>
-                    <div className="dialog">
-                        <p>You need to provide a unique username to identify the account.<br/>Providing a password is optional - if none is provided, a random one will be generated.</p>
-                        <label>
-                            <input type="text" id="newusername" placeholder="New Username"/>
-                        </label>
-                        <label>
-                            <input type="password" id="newpassword" placeholder="New Password"/>
-                        </label>
-                        <br />
-                        <h4>Do you want this account to be an administrator?</h4>
-                        <p>Administrators can edit, create, and delete questions.</p>
-                        <p>Administrators can also manage other accounts using this page.</p>
-                        <p>Grant this permission carefully!</p>
-                        <label>
-                            <input type="checkbox" id="newadmin" className="select-box"/>
-                        </label>
-                        <hr />
-                        <p>In order to authorize the creation of a new account, please re-enter your password.</p>
-                        <input className="loginbox" type="password" id="password" placeholder="Password" onKeyDown={handleEnterKey}/>
-                    </div>
-                    <button className="mainbutton" onClick={() => handleCreateAccount(document.getElementById("password").value)}>Create Account</button>
                 </div>
             );
         }
@@ -128,9 +252,11 @@ export function ManageAccounts() {
         if (user != undefined && user.uid > 0 && user.admin) {
             return (
             <div className="midbound">
+                <img src="loading.gif" className="loading-symbol" id="login-loading"/>
                 <h4>Manage Accounts</h4>
                 <p>Admins can use this page to create, reset password for, and delete user accounts for Quizpedia. Click on any account to view options for it.</p>
                 <AccountDisplay />
+                <button className="mainbutton" onClick={() => window.location.href = "/"}>Back to Home</button>
             </div>
             );
         } else {
